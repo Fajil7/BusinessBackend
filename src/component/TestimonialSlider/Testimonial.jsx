31d13b493ab525c3e1a90086b1719da86a1d6a86
fajil <fajil335@gmail.com>
2024-06-22 18:15:23 +0530

    cf

--- src/component/TestimonialSlider/Testimonial.jsx
+++ src/component/TestimonialSlider/Testimonial.jsx
@@ -30,39 +30,53 @@ const TestimonialSlider = () => {
   };
 
   return (
-    <Slider {...settings}>
-      <div className='testimonial-container flex flex-col items-center'>
-        <div className='margin-container flex flex-col items-center'>
-          <img
-            src="https://businesssphere.info/uploads/testimonial/8367771856983015.png"
-            class="testimonial-img"
-          />
-          <p class="tesi-into">
-            My name is Sanjay Kumar Jha, founder of MSME DOST and SIMPLIFIED. We are into Website,
-            Software and Technology aspect of compliances. We have got a few clients from this contact
-            sphere and also a few solutions and
-            services I am availing from members of Business Sphere. This is really useful and helpful.
-          </p>
-          <h6 class="testi-name"> Sanjay Kumar Jha</h6>
-          <span class="testi-possition"> MSMEDOST and Simplified Management Solutions</span>
+    <>
+      <div className="cards">
+        <div className="outer">
+          <div className="card" style={{ '--delay': '-1' }}>
+            <div className="header">
+              <div className="profile">
+                <div className="img">
+                  <img src="https://businesssphere.info/uploads/testimonial/8367771856983015.png" />
+                  <div className="details">
+                    <h4>Sanjay Jha</h4>
 
+                  </div>
+                </div>
+              </div>
+              <div className="testimonial">
+                <p> My name is Sanjay Kumar Jha, founder of MSME DOST and SIMPLIFIED. We are into Website,
+                  Software and Technology aspect of compliances. We have got a few clients from this contact
+                  sphere and also a few solutions and
+                  services I am availing from members of Business Sphere. This is really useful and helpful.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="card" style={{ '--delay': '0' }}>
+            <div className="header">
+              <div className="profile">
+                <div className="img">
+                  <img src="https://businesssphere.info/uploads/testimonial/804084144243608.jpg" />
+                  <div className="details">
+                    <h4>Shubham Jain</h4>
+
+                  </div>
+                </div>
+              </div>
+              <div className="testimonial">
+                <p> My name is Sanjay Kumar Jha, founder of MSME DOST and SIMPLIFIED. We are into Website,
+                  Software and Technology aspect of compliances. We have got a few clients from this contact
+                  sphere and also a few solutions and
+                  services I am availing from members of Business Sphere. This is really useful and helpful.
+                </p>
+              </div>
+            </div>
+          </div>
+          
         </div>
       </div>
-      <div className='testimonial-container'>
-        <div className='margin-container flex flex-col items-center'>
-          <img
-            src="	https://businesssphere.info/uploads/testimonial/804084144243608.jpg"
-            class="testimonial-img"
-          />
-          <p class="tesi-into">
-          I am Shubham Jain from S.P. Hosiery and I am satisfied with this forum. I have got Business connects and referrals and got few Businesses too. Thanks to the member of the forum and I am happy with forums activities. 
-          </p>
-          <h6 class="testi-name">Shubham Jain</h6>
-          <span class="testi-possition">S.P.Jain Hosiery</span>
-
-        </div>
-      </div>
-    </Slider>
+    </>
   );
 };
 
