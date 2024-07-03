d0adc020cbb1522f7a190e83201f8f5b2ffb79c3
fajil <fajil335@gmail.com>
2024-07-03 17:39:12 +0530

    j

--- src/component/Popup/Popup.jsx
+++ src/component/Popup/Popup.jsx
@@ -4,6 +4,12 @@ import './Popup.css'; // Import your popup CSS file
 const Popup = ({ messages, onClose }) => {
     return (
         <div className="popup">
+            <div>
+                <img 
+                src='https://img.freepik.com/free-vector/colorful-balloons-happy-birthday-card_1017-32692.jpg?t=st=1720007710~exp=1720011310~hmac=63ab89e05faddeb2ce5fbb6a268bd81a16a0bd8b10f0bba8d2e2745bc5456851&w=740'
+                className='birthday-image'
+                />
+            </div>
             <div className="popup-content">
                 <span className="close" onClick={onClose}>&times;</span>
                 {messages.map((message, index) => (
